f0e7257ca179201f6de6b4ea2029d23627a7965d
Azizbek-a <jamolovazizbek261@gmail.com>
2025-09-18 19:39:11 +0500

    Add files via upload

---
+++ Aziz.py
@@ -0,0 +1,18 @@
+son1=25
+son2=13
+print(son1+son2)
+print(son1-son2)
+print(son1*son2)
+print(son1/son2)
+ism="Azizbek"
+familya="Jamolov"
+yosh=13
+print(f" Mening ismim {ism} mening familyam {familya} mening yoshim {yosh}")
+print(son1/son2)
+print(son1//son2)
+p=3.14
+r=28
+print(p*r*2)
+print(p*28**2)
+print(5**2)
+print(5**3)
